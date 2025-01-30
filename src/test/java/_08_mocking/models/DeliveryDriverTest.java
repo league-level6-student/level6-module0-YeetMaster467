c7fa5d17dab7539d905300d5da4e75800c1ab665
YeetMaster467 <elicomixinc@gmail.com>
2025-01-29 19:56:36 -0800

    Completed DeliveryDriverTest
    Intro to white box testing completed

--- src/test/java/_08_mocking/models/DeliveryDriverTest.java
+++ src/test/java/_08_mocking/models/DeliveryDriverTest.java
@@ -13,36 +13,60 @@ class DeliveryDriverTest {
 
     DeliveryDriver deliveryDriver;
 
+
+    String name = "James";
+
+    @Mock
+    Car car;
+
+    @Mock
+    CellPhone cellPhone;
+
     @BeforeEach
     void setUp() {
-
+        MockitoAnnotations.openMocks(this);
+        deliveryDriver = new DeliveryDriver(name, car, cellPhone);
     }
 
     @Test
     void itShouldWasteTime() {
         //given
+        boolean expected = true;
 
         //when
+        when(deliveryDriver.wasteTime()).thenReturn(true);
+        boolean actual = deliveryDriver.wasteTime();
 
         //then
+        assertEquals(expected, actual);
     }
 
     @Test
     void itShouldRefuel() {
         //given
+        int octaneGrade = 85;
+        boolean expected = true;
 
         //when
+        when(deliveryDriver.refuel(85)).thenReturn(true);
+        boolean actual = deliveryDriver.refuel(85);
 
         //then
+        assertEquals(expected, actual);
     }
 
     @Test
     void itShouldContactCustomer() {
         //given
+        boolean expected = true;
+        String phoneNumber = "411";
+        when(deliveryDriver.contactCustomer(phoneNumber)).thenReturn(true);
 
         //when
+        boolean actual = deliveryDriver.contactCustomer(phoneNumber);
 
         //then
+        assertEquals(expected, actual);
     }
 
 }
