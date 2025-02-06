49afa54aedf5bc78cff2ad8fee1a362d75b2ee77
YeetMaster467 <elicomixinc@gmail.com>
2025-02-05 20:01:34 -0800

    My Donut Shop in Progress

--- src/test/java/_10_white_box_testing/MyDonutShopTest.java
+++ src/test/java/_10_white_box_testing/MyDonutShopTest.java
@@ -1,5 +1,6 @@
 package _10_white_box_testing;
 
+import _08_mocking.models.DeliveryDriver;
 import _09_intro_to_white_box_testing.models.DeliveryService;
 import _09_intro_to_white_box_testing.models.Order;
 import _10_white_box_testing.models.BakeryService;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -16,19 +20,37 @@ import static org.mockito.Mockito.*;
 class MyDonutShopTest {
 
     MyDonutShop myDonutShop;
+    PaymentService paymentService;
+    DeliveryService deliveryService;
+    BakeryService bakeryService;
+
+    DeliveryDriver deliveryDriver;
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
 
+        List<DeliveryDriver> availableDeliveryDrivers = Collections.singletonList(deliveryDriver);
+
+        paymentService = new PaymentService();
+        deliveryService = new DeliveryService(availableDeliveryDrivers);
+        bakeryService = new BakeryService();
+        myDonutShop = new MyDonutShop(paymentService, deliveryService, bakeryService);
+
+        myDonutShop.openForTheDay();
     }
 
     @Test
     void itShouldTakeDeliveryOrder() throws Exception {
+
         //given
+        Order order = new Order("James", "411", 6, 10, "3", true);
 
         //when
+        myDonutShop.takeOrder(order);
 
         //then
+        verify(myDonutShop, times(1)).takeOrder(order);
     }
 
     @Test
